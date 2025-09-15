34556d8740477f9ff8b272298a446135541f831b
Jean Silva <jjeansilva@gmail.com>
2025-09-15 19:15:33 +0000

    Não foi solucionado. Explore a segunda possibilidade e me diga quais as

--- src/components/tables/data-table-faceted-filter.tsx
+++ src/components/tables/data-table-faceted-filter.tsx
@@ -84,7 +84,7 @@ export function DataTableFacetedFilter<TData, TValue>({
           )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0" align="start">
+      <PopoverContent className="w-[200px] p-0 z-50" align="start">
         <Command>
           <CommandInput placeholder={title} />
           <CommandList>
@@ -112,7 +112,7 @@ export function DataTableFacetedFilter<TData, TValue>({
                         "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
                         isSelected
                           ? "bg-primary text-primary-foreground"
-                          : "opacity-50 [&_svg]:invisible"
+                          : "[&_svg]:invisible"
                       )}
                     >
                       <Check className="h-4 w-4" />
